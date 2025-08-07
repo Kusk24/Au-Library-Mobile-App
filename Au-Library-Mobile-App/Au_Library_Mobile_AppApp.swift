18c9ccfe11444043c87ebbfc00d3ac71634a6c0e
Kusk24 <winyumg2003a@gmail.com>
2025-08-07 22:56:58 +0700

    Init SwiftUI project

---
+++ Au-Library-Mobile-App/Au_Library_Mobile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Au_Library_Mobile_AppApp.swift
+//  Au-Library-Mobile-App
+//
+//  Created by Win Yu Maung on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Au_Library_Mobile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
