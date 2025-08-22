87e1c91ad0c137713be58f9faca9513d3a88f061
MinThetN <minthetn15@gmail.com>
2025-08-22 16:19:29 +0700

    created noti page

---
+++ AuLibraryMobileApp/NotificationPage.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationPage.swift
+//  AuLibraryMobileApp
+//
+//  Created by Min Thet Naung on 22/08/2025.
+//
+
+import SwiftUI
+
+struct NotificationPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationPage()
+}
