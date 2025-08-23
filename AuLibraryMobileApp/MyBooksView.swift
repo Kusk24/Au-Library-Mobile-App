7f4da617b0c8e2217143ee08bc42fbc478cf8348
Thiri Htet <thirihtet7864@gmail.com>
2025-08-23 19:27:40 +0700

    add segmented control in mybooksview

--- AuLibraryMobileApp/MyBooksView.swift
+++ AuLibraryMobileApp/MyBooksView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct MyBooksView: View {
+    
+    @State var selected: BookStatus = .all
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading){
+                Text("My Books")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.red)
+                
+                Picker("Filter", selection: $selected){
+                    ForEach(BookStatus.allCases){ s in
+                        Text(s.title).tag(s)}
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                
+            
+            }
+        }
+        Spacer()
     }
 }
 
