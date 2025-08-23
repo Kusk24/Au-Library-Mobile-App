2919afee671510da712df866f20a3c29b22edf73
Kusk24 <winyumg2003a@gmail.com>
2025-08-23 09:33:51 +0700

    feat: Added  Detail Page UI

--- AuLibraryMobileApp/DetailPage.swift
+++ AuLibraryMobileApp/DetailPage.swift
@@ -9,26 +9,50 @@ import SwiftUI
 
 struct DetailPage: View {
     var body: some View {
-        VStack{
-            HStack{
-                Image(systemName: "book")
-                    .frame(width: 100, height: 100)
+        VStack(){
+            HStack(spacing: 40){
+                Image(systemName: "book.closed")
+                    .frame(width: 100, height: 120)
                     .background(.gray)
-                VStack{
-                    Text("Title")
-                    Text("author")
-                    Text("edition")
-                    Text("date")
-                    Text("imprint publisher")
+                
+                VStack(alignment: .leading, spacing: 10){
+                    Text("Title: ").font(.caption)
+                    Text("author: ").font(.caption)
+                    Text("edition: ").font(.caption)
+                    Text("date: ").font(.caption)
+                    Text("imprint publisher: ").font(.caption)
                 }
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 40)
+            .padding(.vertical, 80)
             
             Text("Title is written by author, it is the edition th, pulished in date by imprint. The books is about call no.")
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
             
             Text("Location: call no.")
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
             
-            Text("Status")
-        }
+            
+            Spacer()
+            
+            Text("Status: Available")
+            
+            
+            Button("Add to Favorite"){
+                print("Added to favorite")
+            }.frame(width: 200, height: 50)
+                .background(Color(.systemRed))
+                .clipShape(.capsule)
+                .foregroundStyle(Color(.white))
+                .padding(.bottom, 16)
+            
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
 
